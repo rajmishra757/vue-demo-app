0f19d315aa3e510dd18af936aa7212ac13e9e29c
Rajalaaxmi <rajmishra757@gmail.com>
2018-03-11 23:08:08 +0530

    Login page added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <main-page/>
   </div>
 </template>
 
 <script>
+import MainPage from './components/MainPage';
+
 export default {
-  name: 'App'
-}
+  name: 'App',
+  components: {
+    'main-page': MainPage,
+  },
+};
 </script>
 
 <style>
@@ -18,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id='main-page' class='container-fluid'>
+    <div class='row content'>
+        <div class='col-lg-6 col-md-6 col-sm-12 col-xs-12
+          bg-left text-center main-page-section'>
+            <img src='../assets/vue.jpg'
+              class='img-responsive img-circle'
+              alt='Logo' width='350' height='350'/>
+            <h3>A demo application using Vue.js</h3>
+        </div>
+        <div class='col-lg-6 col-md-6 col-sm-12 col-xs-12
+          bg-right text-center main-page-section'>
+          <div class='form-container'>
+            <h2>Login</h2>
+            <form class='form-horizontal' action=''>
+              <div class='form-group'>
+                <label class='control-label col-sm-2' for='username'>Username:</label>
+                <div class='col-sm-10'>
+                  <input type='username' class='form-control' id='username' placeholder='Enter username' name='username'>
+                </div>
+              </div>
+              <div class='form-group'>
+                <label class='control-label col-sm-2' for='password'>Password:</label>
+                <div class='col-sm-10'>          
+                  <input type='password' class='form-control' id='password' placeholder='Enter password' name='password'>
+                </div>
+              </div>
+              <div class='form-group'>        
+                <div class='col-sm-offset-2 col-sm-10'>
+                  <button type='submit' class='btn btn-info pull-right'>Login</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+};
+</script>
+
+
+<style scoped>
+.main-page-section {
+  padding-top: 70px;
+  padding-bottom: 70px;
+}
+.row {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+}
+.row > [class*='col-'] {
+  display: flex;
+  flex-direction: column;
+}
+.margin {
+  margin-bottom: 45px;
+}
+.bg-left {
+  background-color: #1abc9c; /* Green */
+  color: #ffffff;
+}
+.bg-right {
+  background-color: #474e5d; /* Dark Blue */
+  color: #ffffff;
+}
+#main-page img {
+  margin: auto;
+  display: inline;
+}
+.form-container {
+  margin: auto;
+  width: 100%;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+#main-page form {
+  padding-top: 30px;
+}
+</style>
+
