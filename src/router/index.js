a35d1eef0c63f5b101bfa22b2acfa69b2f0d3644
Rajalaxmi Mishra <rajmishra757@gmail.com>
2018-03-12 18:49:20 +0530

    Home page created

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import HomePage from '@/components/HomePage'
 
 Vue.use(Router)
 
@@ -14,8 +15,8 @@ export default new Router({
     },
     {
       path: '/home',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'HomePage',
+      component: HomePage
     }
   ]
 })
